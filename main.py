81e3d77f423bb71164e827a7144b9da5daa74ad1
M-Younus <younus.ubit56@gmail.com>
2018-09-20 10:02:15 +0500

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 20 09:34:04 2018
+
+@author: MY
+"""
+
+#%% Loading Data
+
+import pandas as pd
+train=pd.read_csv('train.csv')
+test=pd.read_csv('test.csv')
+
+#%% Univariate Analysis
+
+
+
